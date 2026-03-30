e3afbed6edf4b85e7f3b93df967c1de14e9b99bc
Marissa D'Souza <marissadsouzaa@gmail.com>
2026-03-29 21:31:40 -0700

    03-29-2026 fixes
    new site

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,37 @@
 
+/*-------------------------------------------------------------------------------
+  PRE LOADER + IMAGE LOADING HINTS
+-------------------------------------------------------------------------------*/
 
-  /*-------------------------------------------------------------------------------
-    PRE LOADER
-  -------------------------------------------------------------------------------*/
+(function ($) {
+  function hidePreloader() {
+    $('.preloader').fadeOut(400);
+  }
 
-  $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+  function optimizeImages() {
+    $('img').each(function (index, img) {
+      if (!img.hasAttribute('decoding')) {
+        img.setAttribute('decoding', 'async');
+      }
+
+      if (!img.hasAttribute('loading') && index > 1) {
+        img.setAttribute('loading', 'lazy');
+      }
+    });
+  }
+
+  $(optimizeImages);
+
+  $('.navbar-collapse a').on('click', function () {
+    var $toggle = $('.navbar-toggle');
+    if ($toggle.is(':visible')) {
+      $('.navbar-collapse').collapse('hide');
+    }
   });
 
+  if (document.readyState === 'complete') {
+    hidePreloader();
+  } else {
+    $(window).on('load', hidePreloader);
+  }
+})(jQuery);
